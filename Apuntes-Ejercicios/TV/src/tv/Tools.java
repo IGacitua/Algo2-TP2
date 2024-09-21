989d6c09f1ba7f982cafcbbff5c59ae8ab63271c
Usuario <Usuario@DESKTOP-C2K5E38>
2024-09-20 21:09:24 -0300

    Agrego Pre y Post de los metodos de Televisor, Canal y Tools Agrego mensajes para verificar ciertas partes del codigo

--- Apuntes-Ejercicios/TV/src/tv/Tools.java
+++ Apuntes-Ejercicios/TV/src/tv/Tools.java
@@ -1,11 +1,23 @@
 package tv;
 
 public class Tools {
-
+	
+	/**
+	 * pre: -
+	 * post: Devuelve un booleano (true) indicando si es positivo o 0 el valor dado como dato
+	 * @param value, valor entero
+	 * @return booleano true or false
+	 */
 	public static boolean checkPositive(int value) {
 		return value >= 0;
 	}
-
+	
+	/**
+	 * pre: -
+	 * post: Devuelve un booleano (true) indicando si el valor dado como parametro es positivo estricto
+	 * @param value, un valor entero
+	 * @return booleano true or false
+	 */
 	public static boolean checkPositiveStrict(int value) {
 		return value > 0;
 	}
