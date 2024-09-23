36b8ee2257e2dea516a28a9d8dd07ac9b3848bd0
IGacitua <ezmagacitua@gmail.com>
2024-09-23 19:33:00 -0300

    Cambios en base a los tips dados por el profesor

--- Apuntes-Ejercicios/TV/src/tv/MainTesteo.java
+++ Apuntes-Ejercicios/TV/src/tv/MainTesteo.java
@@ -2,87 +2,87 @@ package tv;
 
 public class MainTesteo {
 
-	public static void main(String[] args) throws Exception {
-		//Crea un televisor con 140 canales
-		Televisor televisor = new Televisor(140);
-		
-		//Prueba setear un canal e ir subiendo y bajando de canal y volumen
-		televisor.setCanal(4);
-		System.out.println("El canal actual es " + televisor.getCanalActual() + " y el volumen actual es " + televisor.getVolumenActual());
-		for (int i = 0; i < 50; i++) {
-			televisor.subirVolumen();
-		}
-		System.out.println("El volumen del canal actual es " + televisor.getVolumenCanal(televisor.getCanalActual()));
-		System.out.println("Ahora el volumen actual es " + televisor.getVolumenActual());
-		
-		for (int i = 0; i < 12; i++) {
-			televisor.subirCanal();
-			televisor.bajarVolumen();
-		}
-		System.out.println("");
-		System.out.println("Subí el canal varias veces y bajé el volumen, ahora el canal al que se cambió es el " + televisor.getCanalActual() + " y el volumen es " + televisor.getVolumenCanal(televisor.getCanalActual()));
-		System.out.println("");
-		
-		//Prueba el silenciar el televisor
-		System.out.println("¿El Televisor está silenciado? -> " + televisor.getSilencio());
-		televisor.switchSilencio();
-		System.out.println("¿Y ahora? -> " + televisor.getSilencio());
-		System.out.println("Entonces el volumen actual es: " + televisor.getVolumenActual());
-		televisor.switchSilencio();
-		System.out.println("Ya no está silenciado, entonces su volumen es: " + televisor.getVolumenActual());
-		System.out.println("");
-		
-		//Más pruebas con bucles
-		for (int i = 0; i < 100; i++) {
-			televisor.subirCanal();
-		}
-		while (televisor.getCanalActual() > 99) {
-			televisor.bajarCanal();
-		}
-		System.out.println("El canal al que se cambió es el " + televisor.getCanalActual());
-		System.out.println("");
-		
-		//Prueba al silenciar, subir el volumen y desilenciar
-		System.out.println("El volumen es " + televisor.getVolumenActual());
-		televisor.switchSilencio();
-		televisor.subirVolumen();
-		System.out.println("Tras silenciar y subir el volumen, debería desilenciarse y subir uno. Entonces el volumen es: " + televisor.getVolumenActual());
-		System.out.println("");
-		
-		//Prueba de excepciones
-			//Intentar un canal inválido
+    public static void main(String[] args) throws Exception {
+        //Crea un televisor con 140 canales
+        Televisor televisor = new Televisor(140);
+
+        //Prueba setear un canal e ir subiendo y bajando de canal y volumen
+        televisor.canalEstablecer(4);
+        System.out.println("El canal actual es " + televisor.canalActualObtener() + " y el volumen actual es " + televisor.volumenActualObtener());
+        for (int i = 0; i < 50; i++) {
+            televisor.volumenSubir();
+        }
+        System.out.println("El volumen del canal actual es " + televisor.getVolumenCanal(televisor.canalActualObtener()));
+        System.out.println("Ahora el volumen actual es " + televisor.volumenActualObtener());
+
+        for (int i = 0; i < 12; i++) {
+            televisor.canalSubir();
+            televisor.volumenBajar();
+        }
+        System.out.println("");
+        System.out.println("Subí el canal varias veces y bajé el volumen, ahora el canal al que se cambió es el " + televisor.canalActualObtener() + " y el volumen es " + televisor.getVolumenCanal(televisor.canalActualObtener()));
+        System.out.println("");
+
+        //Prueba el silenciar el televisor
+        System.out.println("¿El Televisor está silenciado? -> " + televisor.silencioObtener());
+        televisor.silencioAlternar();
+        System.out.println("¿Y ahora? -> " + televisor.silencioObtener());
+        System.out.println("Entonces el volumen actual es: " + televisor.volumenActualObtener());
+        televisor.silencioAlternar();
+        System.out.println("Ya no está silenciado, entonces su volumen es: " + televisor.volumenActualObtener());
+        System.out.println("");
+
+        //Más pruebas con bucles
+        for (int i = 0; i < 100; i++) {
+            televisor.canalSubir();
+        }
+        while (televisor.canalActualObtener() > 99) {
+            televisor.canalBajar();
+        }
+        System.out.println("El canal al que se cambió es el " + televisor.canalActualObtener());
+        System.out.println("");
+
+        //Prueba al silenciar, subir el volumen y desilenciar
+        System.out.println("El volumen es " + televisor.volumenActualObtener());
+        televisor.silencioAlternar();
+        televisor.volumenSubir();
+        System.out.println("Tras silenciar y subir el volumen, debería desilenciarse y subir uno. Entonces el volumen es: " + televisor.volumenActualObtener());
+        System.out.println("");
+
+        //Prueba de excepciones
+        //Intentar un canal inválido
         try {
-            televisor.setCanal(1000);
+            televisor.canalEstablecer(1000);
         } catch (Exception e) {
             System.out.println("Error al cambiar de canal: " + e.getMessage());
         }
-        
-        	//Subir demasiadas veces un canal
+
+        //Subir demasiadas veces un canal
         try {
             for (int i = 0; i < 10000; i++) {
-            	televisor.subirCanal();
+                televisor.canalSubir();
             }
         } catch (Exception e) {
             System.out.println("Error al subir el canal: " + e.getMessage());
         }
-        
-        	//Subir el volumen demasiado
+
+        //Subir el volumen demasiado
         try {
-        	for (int i = 0; i < 300; i++) {
-        		televisor.subirVolumen();
+            for (int i = 0; i < 300; i++) {
+                televisor.volumenSubir();
             }
         } catch (Exception e) {
             System.out.println("Error al subir el volumen: " + e.getMessage());
         }
-        
-        	//Bajar el volumen demasiado
+
+        //Bajar el volumen demasiado
         try {
-        	for (int i = 0; i < 300; i++) {
-        		televisor.bajarVolumen();
+            for (int i = 0; i < 300; i++) {
+                televisor.volumenBajar();
             }
         } catch (Exception e) {
             System.out.println("Error al bajar el volumen: " + e.getMessage());
         }
-	}
+    }
 
 }
--- Apuntes-Ejercicios/TV/src/tv/Televisor.java
+++ Apuntes-Ejercicios/TV/src/tv/Televisor.java
@@ -1,190 +1,213 @@
 package tv;
 
 public class Televisor {
-	
-	// Atributos -------------------------------------------------------------------------------------------------------
-	
-	private Canal[] canales = null;
-	private int volumenActual = 0;
-	//inicializamos el valor en 1 indicando el primer canal
-	private int canalActual = 1;
-	private boolean silenciado = false;
-	
-	/**
-	 * pre: -
-	 * post: Devuelve un televisor con cantidadCanales inicializados, con volumen actual en 0, canal actual en 1 y no silenciado
-	 * @param cantidadCanales, un valor entero
-	 * @throws Exception si el valor dado como parametro es 0 o negativo
-	 */
-	public Televisor(int cantidadCanales) throws Exception {
-		if(!(Tools.checkPositiveStrict(cantidadCanales))) {
-			throw new Exception("Cantidad de canales invalido");
-		}
-		this.canales = new Canal[cantidadCanales];
-		for (int i = 0; i < cantidadCanales; i++) {
-			this.canales[i] = new Canal(i + 1); // + 1 para que sea empezando en 1.
-		}
-	}
-	
-	/**
-	 * pre: -
-	 * post: Sube de canal
-	 * @throws Exception si estamos en el canal maximo
-	 */
-	public void subirCanal() throws Exception {
-		setCanal(canalActual + 1);
-	};
-	
-	/**
-	 * pre: -
-	 * post: Baja de canal
-	 * @throws Exception si estamos en el canal 1
-	 */
-	public void bajarCanal() throws Exception {
-		setCanal(canalActual - 1);
-	};
-	
-	/**
-	 * pre: -
-	 * post: Cambia de canal al indicado como parametro
-	 * @param canal, un valor entero que sea el canal al que se quiera cambiar
-	 * @throws Exception si es un canal invalido
-	 */
-	public void setCanal(int canal) throws Exception {
-		verificarValidezCanal(canal);
-		this.canalActual = canal;
-		//cambiamos el volumen del canal al que cambiamos
-		actualizarVolumenCanalActual();
-	};
-	
-	/**
-	 * pre: -
-	 * post: Sube en 1 el volumen actual
-	 * @throws Exception si el volumen es maximo (100)
-	 */
-	public void subirVolumen() throws Exception {
-		if (silenciado) {
-            silenciado = false; // desmutea
+
+    // Atributos -------------------------------------------------------------------------------------------------------
+    private Canal[] canales = null;
+    private int volumenActual = 0;
+    private int canalActual = 1; // El profesor sugiere utilizar un puntero al canal, pero no rompería con la encapsulación? ~Gacitua
+    private boolean silenciado = false; // El profesor sugiere un enum, pero creería que un boolean es mejor ~Gacitua
+    private final int VOLUMEN_MAXIMO_POSIBLE = 100;
+
+    /**
+     * pre: - post: Devuelve un televisor con cantidadCanales inicializados, con
+     * volumen actual en 0, canal actual en 1 y no silenciado
+     *
+     * @param cantidadCanales, un valor entero
+     * @throws Exception si el valor dado como parametro es 0 o negativo
+     */
+    public Televisor(int cantidadCanales) throws Exception {
+        if (!(Tools.revisarPositivoEstricto(cantidadCanales))) {
+            throw new Exception("Cantidad de canales invalido");
         }
-		if (this.volumenActual < 100) {
-			this.volumenActual += 1;
-			actualizarVolumenCanalActual();
-		} else {
-			throw new Exception("Volumen no puede ser mayor a 100.");
-		}
-	};
-	
-	/**
-	 * pre: -
-	 * post: Baja el volumen
-	 * @throws Exception si el volumen es minimo
-	 */
-	public void bajarVolumen() throws Exception {
-		if (silenciado) {
-            silenciado = false; // desmutea
+        this.canales = new Canal[cantidadCanales];
+        for (int i = 0; i < cantidadCanales; i++) {
+            this.canales[i] = new Canal(i + 1); // + 1 para que sea empezando en 1 el NUMERO del canal
         }
-		if (this.volumenActual > 0) {
-			this.volumenActual -= 1;
-			actualizarVolumenCanalActual();
-		} else {
-			throw new Exception("Volumen no puede ser menor a 0.");
-		}
-	};
-	
-	/**
-	 * pre: -
-	 * post: Mutea o desmutea el televisor viendo su estado anterior
-	 */
-	public void switchSilencio() {
-		this.silenciado = !this.silenciado;
-	};
-	
-	/**
-	 * pre: -
-	 * post: devuelve un booleano indicando si el televisor esta muteado o en estado normal
-	 * @return un booleando true or false
-	 */
-	public boolean getSilencio() {
-		return this.silenciado;
-	}
-	
-	/**
-	 * pre: -
-	 * post: Devuelve el valor del canal actual
-	 * @return un entero que representa el canal actual
-	 */
-	public int getCanalActual() {
-		return this.canalActual;
-	}
-	
-	/**
-	 * pre: -
-	 * post: -
-	 * @return devuelve el valor actual del volumen
-	 */
-	public int getVolumenActual() {
-		if (silenciado) {
-			return 0;
-		}
-		return this.volumenActual;
-	}
-	
-	/**
-	 * pre: -
-	 * post: Devuelve el numero del canal que registro el mayor volumen
-	 * @return un entero que representa el numero del canal con mayor volumen registrado
-	 */
-	public int getCanalMaxVol() {
-		int maximoEncontrado = 0;
-		for (int i = 0; i < this.canales.length; i++) {
-			if (this.canales[i].getVolumenMaximo() > this.canales[maximoEncontrado].getVolumenMaximo()) {
-				maximoEncontrado = i;
-			}
-		}
-		return maximoEncontrado + 1;
-	}
-	
-	/**
-	 * pre: -
-	 * @param canal, el numero del canal el cual quiere saber el volumen actual
-	 * @return el volumen del canal dado como parametro
-	 * @throws Exception 
-	 */
-	public int getVolumenCanal(int canal) throws Exception {
-		verificarValidezCanal(canal);
-		return this.canales[canal - 1].getVolumenActual();
-	};
-	
-	/**
-	 * pre: -
-	 * @param canal, el numero de canal que se quiere saber el mayor volumen registrado
-	 * @return un entero 
-	 * @throws Exception 
-	 */
-	public int getVolumenMaxCanal(int canal) throws Exception {
-		verificarValidezCanal(canal);
-		return this.canales[canal - 1].getVolumenMaximo();
-	}
-	
-	/**
-	 * pre: -
-	 * post: - 
-	 * @param canal, verifica que sea válido (mayor a 0, menor al máximo)
-	 * @throws Exception
-	 */
-	private void verificarValidezCanal(int canal) throws Exception {
-		if (canal < 1 || canal > this.canales.length) {
-			throw new Exception("Canal debe estar entre 1 y " + this.canales.length);
-		}
-	}
-	
-	/**
-	 * pre: -
-	 * post: -
-	 * Actualiza el volumen del canal actual
-	 * @throws Exception
-	 */
-    private void actualizarVolumenCanalActual() throws Exception {
-        canales[canalActual - 1].changeVolumen(volumenActual);
+    }
+
+    /**
+     * pre: - post: Sube de canal
+     *
+     * @throws Exception Cuando el canal es mayor a cantidadCanales establecido
+     * al crear el objeto
+     */
+    public void canalSubir() throws Exception {
+        canalEstablecer(canalActual + 1);
+    }
+
+    /**
+     * pre: - post: Baja de canal
+     *
+     * @throws Exception Cuando el canal ya es el minimo posible (1)
+     */
+    public void canalBajar() throws Exception {
+        canalEstablecer(canalActual - 1);
+    }
+
+    /**
+     * pre: - post: Cambia de canal al indicado como parametro
+     *
+     * @param canal, El canal al que se desea cambiar. Entre 1 y cantidadCanales
+     * establecido al crear el objeto (Inclusivo)
+     * @throws Exception Cuando el numero del canal es invalido
+     */
+    public void canalEstablecer(int canal) throws Exception {
+        verificarValidezCanal(canal);
+        this.canalActual = canal;
+        canales[canal - 1].volumenActualEstablecer(volumenActual);
+    }
+
+    /**
+     * pre: - post: Sube en 1 el volumen actual
+     *
+     * @throws Exception Cuando el volumen ya es el maximo (100)
+     */
+    public void volumenSubir() throws Exception {
+        volumenEstablecer(volumenActual + 1);
+    }
+
+    /**
+     * pre: - post: Baja en 1 l volumen actual
+     *
+     * @throws Exception Cuando el volumen ya es el minimo (0)
+     */
+    public void volumenBajar() throws Exception {
+        volumenEstablecer(volumenActual - 1);
+    }
+
+    /**
+     * pre: - post: Cambia el volumen actual al volumen dado
+     *
+     * @param volumen, El volumen al que se desea cambiar. Entre 0 y 100,
+     * inclusivo.
+     * @throws Exception Cuando el volumen está fuera del rango dado
+     */
+    private void volumenEstablecer(int volumen) throws Exception {
+        if (silenciado) {
+            silenciado = false; // Desmutea
+        }
+        if (this.volumenActual > 0 || this.volumenActual < VOLUMEN_MAXIMO_POSIBLE) {
+            this.volumenActual = volumen;
+            canales[canalActual - 1].volumenActualEstablecer(volumenActual);
+        } else {
+            throw new Exception("El volumen dado no es válido");
+        }
+    }
+
+    /**
+     * pre: - post: Mutea o desmutea el televisor dependiendo de su estado
+     * anterior
+     */
+    public void silencioAlternar() {
+        this.silenciado = !this.silenciado;
+    }
+
+    /**
+     * pre: - post: Devuelve un boolean indicando si el televisor esta
+     * silenciado o no
+     *
+     * @return un booleando true or false
+     */
+    public boolean silencioObtener() {
+        return this.silenciado;
+    }
+
+    /**
+     * pre: - post: Devuelve una copia del canal actual
+     *
+     * @return Una copia del canal actual
+     */
+    public Canal canalActualObtener() {
+        Canal canalDevolver = null;
+        try {
+            canalDevolver = canalObtener(canalActual);
+        } catch (Exception e) {
+            // No debería suceder nunca
+        }
+        return canalDevolver;
+    }
+
+    /**
+     * pre: - post: Devuelve el valor del canal actual
+     *
+     * @return Un entero que representa el canal actual
+     */
+    public int canalActualObtenerIndice() {
+        return this.canalActual;
+    }
+
+    /**
+     * pre: - post: -
+     *
+     * @return devuelve el valor actual del volumen
+     */
+    public int volumenActualObtener() {
+        if (silenciado) {
+            return 0;
+        }
+        return this.volumenActual;
+    }
+
+    /**
+     * pre: - post: Devuelve una copia del canal que registro el mayor volumen
+     *
+     * @return una copia del canal que registró el mayor volumen registrado
+     */
+    public Canal canalDeVolumenMaximoObtener() {
+        int maximoEncontrado = 0;
+        for (int i = 0; i < this.canales.length; i++) {
+            if (this.canales[i].volumenMaximoObtener() > this.canales[maximoEncontrado].volumenMaximoObtener()) {
+                maximoEncontrado = i;
+            }
+        }
+        Canal canalDevolver = null;
+        try {
+            canalDevolver = canalObtener(maximoEncontrado + 1);
+        } catch (Exception e) {
+            // No debería devolver nunca excepcion
+        }
+        return canalDevolver;
+    }
+
+    /**
+     * pre: -
+     *
+     * @param canal, el numero de canal que se quiere saber el mayor volumen
+     * registrado
+     * @return un entero
+     * @throws Exception
+     */
+    public int canalObtenerVolumenMaximo(int canal) throws Exception {
+        verificarValidezCanal(canal);
+        return this.canales[canal - 1].volumenMaximoObtener();
+    }
+
+    /**
+     * pre: - post: -
+     *
+     * @param canal, verifica que sea válido (mayor a 0, menor al máximo)
+     * @throws Exception
+     */
+    private void verificarValidezCanal(int canal) throws Exception {
+        if (canal < 1 || canal > this.canales.length) {
+            throw new Exception("Canal debe estar entre 1 y " + this.canales.length);
+        }
+    }
+
+    /**
+     * pre: - post: - Devuelve una copia del canal en posición
+     *
+     * @return Una copia del canal en la posición dada
+     */
+    public Canal canalObtener(int numero) throws Exception {
+        if (!(numero > this.canales.length)) {
+            Canal canalDevolver = new Canal(numero);
+            canalDevolver.volumenActualEstablecer(this.canales[numero - 1].volumenActualObtener());
+            canalDevolver.volumenMaximoEstablecer(this.canales[numero - 1].volumenMaximoObtener());
+            return canalDevolver;
+        } else {
+            throw new Exception("El canal dado no existe");
+        }
     }
 }
