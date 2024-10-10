79698492f5e539a7d1286c6a45b07fcc96a87d3d
IGacitua <ezmagacitua@gmail.com>
2024-10-10 20:01:30 -0300

    Añadido a Tests un for loop que llena el tablero

--- Tateti/src/tateti/Test.java
+++ Tateti/src/tateti/Test.java
@@ -4,7 +4,13 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         Tablero tablero = new Tablero(9, 6, 3);
-        tablero.colocarFicha(1, 1, 1, 5);
+        for (int i = 0; i < 256; i++) {
+            int randomPlayer = (int) (Math.random() * 5);
+            int randomPosX = (int) (Math.random() * 8) + 1;
+            int randomPosY = (int) (Math.random() * 6) + 1;
+            int randomPosZ = (int) (Math.random() * 3) + 1;
+            tablero.colocarFicha(randomPosX, randomPosY, randomPosZ, randomPlayer);
+        }
         tablero.imprimirTablero();
     }
 }
