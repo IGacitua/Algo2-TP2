d95065dba438b8d1880acfff961d6f58e09e5be9
IGacitua <ezmagacitua@gmail.com>
2024-10-10 20:05:50 -0300

    Variables de posicion y tamaño en casillero y tablero cambiados a constante. Nombres cambiados para reflejar el cambio.

--- Tateti/src/tateti/Casillero.java
+++ Tateti/src/tateti/Casillero.java
@@ -1,73 +1,65 @@
 package tateti;
 
 public class Casillero {
-	//ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
-	//ATRIBUTOS -----------------------------------------------------------------------------------------------
-	
-	private int posicionX;
-	private int posicionY;
-	private int posicionZ;
-	
-	private int jugador = 0; // 0 sin jugador.
-	private boolean bloqueado = false; // Con ficha -> No se puede sacar. Sin ficha -> No se puede colocar
-	
-	//CONSTRUCTORES -------------------------------------------------------------------------------------------
-	
-	public Casillero(int x, int y, int z) {
-		this.posicionX = x;
-		this.posicionY = y;
-		this.posicionZ = z;
-	}
-	
-	//METODOS DE CLASE ----------------------------------------------------------------------------------------
-	
-	// TODO pre-post
-	public boolean alternarBloqueo() {
-		bloqueado = !bloqueado;
-		return bloqueado;
-	}
-	
-	//METODOS GENERALES ---------------------------------------------------------------------------------------
-	
-	//METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
-	
-	//GETTERS SIMPLES -----------------------------------------------------------------------------------------
-	
-	// TODO todos los pre y post de getters 
-	
-	public int getJugador() {
-		return jugador;
-	}
+    //ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
+    //ATRIBUTOS -----------------------------------------------------------------------------------------------
 
-	public int getPosicionX() {
-		return posicionX;
-	}
+    private final int POSICION_X;
+    private final int POSICION_Y;
+    private final int POSICION_Z;
 
-	public int getPosicionY() {
-		return posicionY;
-	}
+    private int jugador = 0; // 0 sin jugador.
+    private boolean bloqueado = false; // Con ficha -> No se puede sacar. Sin ficha -> No se puede colocar
 
-	public int getPosicionZ() {
-		return posicionZ;
-	}
-	
-	public int[] getCoordenadas() {
-		int[] coordenadas = new int[3];
-		coordenadas[0] = this.posicionX;
-		coordenadas[1] = this.posicionY;
-		coordenadas[2] = this.posicionZ;
-		return coordenadas;
-	}
-	
-	public boolean isBloqueado() {
-		return bloqueado;
-	}
-	
-	
-	//SETTERS SIMPLES -----------------------------------------------------------------------------------------
-	
-	// TODO pre-post
-	public void setJugador(int jugador) {
-		this.jugador = jugador;
-	}
+    //CONSTRUCTORES -------------------------------------------------------------------------------------------
+    public Casillero(int x, int y, int z) {
+        this.POSICION_X = x;
+        this.POSICION_Y = y;
+        this.POSICION_Z = z;
+    }
+
+    //METODOS DE CLASE ----------------------------------------------------------------------------------------
+    // TODO pre-post
+    public boolean alternarBloqueo() {
+        bloqueado = !bloqueado;
+        return bloqueado;
+    }
+
+    //METODOS GENERALES ---------------------------------------------------------------------------------------
+    //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
+    //GETTERS SIMPLES -----------------------------------------------------------------------------------------
+    // TODO todos los pre y post de getters 
+    public int getJugador() {
+        return jugador;
+    }
+
+    public int getPosicionX() {
+        return POSICION_X;
+    }
+
+    public int getPosicionY() {
+        return POSICION_Y;
+    }
+
+    public int getPosicionZ() {
+        return POSICION_Z;
+    }
+
+    public int[] getCoordenadas() {
+        int[] coordenadas = new int[3];
+        coordenadas[0] = this.POSICION_X;
+        coordenadas[1] = this.POSICION_Y;
+        coordenadas[2] = this.POSICION_Z;
+        return coordenadas;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    //SETTERS SIMPLES -----------------------------------------------------------------------------------------
+    // TODO pre-post
+    public void setJugador(int jugador) {
+        this.jugador = jugador;
+    }
 }
