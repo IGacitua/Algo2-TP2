6016ce2532c7fadee734a31020591c9724b02a22
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-10-17 11:17:24 -0300

    Añadida documentación y validaciones

--- Tateti/src/tateti/Jugador.java
+++ Tateti/src/tateti/Jugador.java
@@ -13,22 +13,23 @@ public class Jugador {
 
     //CONSTRUCTORES -------------------------------------------------------------------------------------------
     /**
-     * pre: -, post: - Inicializa Jugador y establece los valores de los
+     * pre: -, post: -
+     * Inicializa Jugador y establece los valores de los
      * atributos
      *
+     * @param nombre: no puede estar vacío
      * @param cantidadJugadores, @param maxCartas: no pueden ser < 0
      * @throws Exception
      */
     public Jugador(String nombre, int identificacion, int maxCartas) throws Exception {
-        // TODO: add identificacion to pre-post
-        if (nombre.isEmpty()) {
-            throw new Exception("El nombre no es valido");
+        if (nombre.trim().isEmpty()) {
+            throw new Exception("El nombre no es válido");
         }
         if (!Herramientas.validarNumeroPositivo(maxCartas)) {
-            throw new Exception("El valor de cartas maximas debe ser mayor o igual que 0");
+            throw new Exception("El valor de cartas máximas debe ser mayor o igual que 0");
         }
         if (!Herramientas.validarNumeroPositivoEstricto(identificacion)) {
-            throw new Exception("El valor de identificacion del usuario debe ser mayor a 0");
+            throw new Exception("El valor de identificación del usuario debe ser mayor a 0");
         }
         this.nombre = nombre;
         this.identificacion = identificacion;
@@ -36,13 +37,20 @@ public class Jugador {
         this.pierdeTurno = false;
     }
 
-    // TODO pre-post (Constructor para jugador sin nombre)
+    /**
+     * pre: -, post: -
+     * Inicializa Jugador sin nombre y establece sus otros atributos
+     * 
+     * @param identificacion: no puede ser <= 0
+     * @param maxCartas: no puede ser < 0
+     * @throws Exception
+     */ //TODO: ELIMINAR
     public Jugador(int identificacion, int maxCartas) throws Exception {
-        if (!Herramientas.validarNumeroPositivo(maxCartas)) {
-            throw new Exception("El valor de cartas maximas debe ser mayor o igual que 0");
+    	if (!Herramientas.validarNumeroPositivoEstricto(identificacion)) {
+            throw new Exception("El valor de identificación del usuario debe ser mayor a 0");
         }
-        if (!Herramientas.validarNumeroPositivoEstricto(identificacion)) {
-            throw new Exception("El valor de identificacion del usuario debe ser mayor a 0");
+        if (!Herramientas.validarNumeroPositivo(maxCartas)) {
+            throw new Exception("El valor de cartas máximas debe ser mayor o igual que 0");
         }
         this.identificacion = identificacion;
         this.maxCartas = maxCartas;
@@ -53,26 +61,47 @@ public class Jugador {
     //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
 
     /**
-     * pre: -, post: - invierte el estado del turno
+     * pre: -, post: - 
+     * Invierte el estado del turno
      */
     public void alternarPierdeTurno() {
         this.pierdeTurno = !this.pierdeTurno;
     }
 
     //GETTERS SIMPLES -----------------------------------------------------------------------------------------
+    /**
+     * pre: -, post: -
+     * @return Devuelve el nombre del jugador si es que tiene
+     * uno asignado, sino devuelve que no se le asignó nada
+     */
     public String getNombreJugador() {
-        // TODO validar si tiene nombre el jugador
+        if (this.nombre == null) {
+        	return "El jugador no tiene un nombre asignado";
+        }
         return nombre;
     }
 
+    /**
+     * pre: -, post: -
+     * @return Devuelve el máximo de cartas en mano
+     */
     public int getMaxCartasEnMano() {
         return maxCartas;
     }
 
+    /**
+     * pre: -, post: -
+     * @return Devuelve la identificacion
+     */
     public int getIdentificacion() {
         return identificacion;
     }
 
+    /**
+     * pre: -, post: -
+     * @return Devuelve un boolean correspondiente a
+     * si pierde el turno o no
+     */
     public boolean isPierdeTurno() {
         return pierdeTurno;
     }
