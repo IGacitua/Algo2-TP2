9085f0e85577f1be71be6e1a829d4105ce06dd14
Tomas Conti <tconti@fi.uba.ar>
2024-10-17 12:22:11 -0300

    Agregado CartaAnularCasillero

---
+++ Tateti/src/tateti/CartaAnularCasillero.java
@@ -0,0 +1,24 @@
+package tateti;
+
+public class CartaAnularCasillero extends Carta {
+
+	public CartaAnularCasillero(Tablero tablero) {
+		super(tablero);
+	}
+	
+	/**
+     * pre: -, post: -
+     * Bloquea un casillero vacio y no bloqueado
+     * 
+     * @param Casillero: debe no estar vacio y no estar bloqueado
+     * @throws Exception
+     */
+	public void usar(Casillero casillero) throws Exception {
+		if (casillero.isVacio() &&
+			!casillero.isBloqueado()) {
+			casillero.alternarBloqueo();
+		} else {
+			throw new Exception("El casillero no esta vacio o ya esta bloqueado");
+		}
+	}
+}
