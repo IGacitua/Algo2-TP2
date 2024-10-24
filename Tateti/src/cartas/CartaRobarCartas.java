5e83c6ac2310bce36378e5afb513b8e2318f329f
Tomas Conti <tconti@fi.uba.ar>
2024-10-24 19:53:32 -0300

    Agregado CartaRobarCartas

---
+++ Tateti/src/cartas/CartaRobarCartas.java
@@ -0,0 +1,29 @@
+package cartas;
+
+import tateti.Jugador;
+import tateti.Mazo;
+
+public class CartaRobarCartas {
+
+	private final static int CANTIDAD_A_ROBAR = 2;
+
+	/**
+     * pre: -, post: -
+     * 
+     * 
+     * @param jugador: jugador que recibira las cartas
+	 * @throws Exception 
+     */
+	public void usar(Jugador jugador, Mazo mazo) throws Exception {
+		if (jugador == null) {
+			throw new Exception("No se especifico un jugador");
+		}
+		if (mazo == null) {
+			throw new Exception("No se especifico un mazo");
+		}
+		if (jugador.getCantidadCartas()+CANTIDAD_A_ROBAR > jugador.getMaxCartasEnMano()) {
+			throw new Exception("No se puede utilizar la carta en este jugador porque excede su maximo");
+		}
+		jugador.robarCartas(CANTIDAD_A_ROBAR, mazo);
+	}
+}
