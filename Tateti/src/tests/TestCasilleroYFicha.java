1d387433246274e3f939caac7fec973140b4bc84
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-02 17:37:54 -0300

    Agregado import Color

--- Tateti/src/tests/TestCasilleroYFicha.java
+++ Tateti/src/tests/TestCasilleroYFicha.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import tateti.Color;
 import tateti.Fichas;
 import tateti.Jugador;
 import tateti.Tablero;
