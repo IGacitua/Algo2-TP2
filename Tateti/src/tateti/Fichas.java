d492bd2bac21dae840fa945cf3a62266109643d6
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-02 18:43:15 -0300

    Agregado obtenerTiposFicha()

--- Tateti/src/tateti/Fichas.java
+++ Tateti/src/tateti/Fichas.java
@@ -1,4 +1,5 @@
 package tateti;
+
 public enum Fichas {
     CUADRADO,
     CIRCULO,
@@ -33,4 +34,20 @@ public enum Fichas {
 			return 'P'; //P de pausa
 		}
 	}
+	
+	/**
+     * pre: -, post: -
+     * @return devuelve un String con todas los tipos de ficha listados
+     */
+    public static String obtenerTiposFicha() {
+    	String tiposExistentes = "";
+    	Fichas[] fichas = Fichas.values();
+    	for (int i = 0; i < fichas.length; i++) {
+    		tiposExistentes += fichas[i].name();
+    		if (i < fichas.length - 1) {
+                tiposExistentes += ", "; //añade una coma y un espacio entre las fichas
+            }
+    	}
+    	return tiposExistentes;
+    }
 }
