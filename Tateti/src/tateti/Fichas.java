bb9830805405ee102b352d8f4d0cabc0149aae1a
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-02 17:33:11 -0300

    Agregado getFichaCaracter()

--- Tateti/src/tateti/Fichas.java
+++ Tateti/src/tateti/Fichas.java
@@ -1,10 +1,36 @@
 package tateti;
-
 public enum Fichas {
     CUADRADO,
     CIRCULO,
     TRIANGULO,
     CRUZ,
-    CUBO,
-    PAUSA
+    RECTANGULO,
+    ESTRELLA,
+    ROMBO,
+    PAUSA;
+
+	/**
+	 * pre: -, post: -
+	 * @return devuelve un char correspondiente a la seleccion
+	 */
+	public char getFichaCaracter() {
+		switch (this) {
+		case CUADRADO:
+			return '■';
+		case CIRCULO:
+			return 'O';
+		case TRIANGULO:
+			return '▲';
+		case CRUZ:
+			return 'X';
+		case RECTANGULO:
+			return '█'; //TODO: CAMBIAR. Pensé en poner este: ▬ pero me parece que es muy similar al cuadrado
+		case ESTRELLA:
+			return '★';
+		case ROMBO:
+			return '♦';
+		default:
+			return 'P'; //P de pausa
+		}
+	}
 }
