28fa842be2ddf5bd68ced7cc3ef8ddfe179b211e
jejberowicz <jejberowicz@fi.uba.ar>
2024-11-06 11:53:18 -0300

    depurando errores

--- Tateti/src/tateti/Menu.java
+++ Tateti/src/tateti/Menu.java
@@ -16,7 +16,6 @@ public class Menu {
 	
 	//constructor
     public Menu(Lista<Jugador> listaJugadores) {
-        this.teclado = new Teclado();
         // Quizas podriamos crear la lista aca mismo, debido a que se cargan los jugadores, asi que la estas creando 			practicamente
         this.listaJugadores = listaJugadores;
     }
@@ -28,7 +27,7 @@ public class Menu {
 	public void cargarJugadores(){
 		int cant_jugadores;
 		System.out.println("Ingrese el numero de jugadores: ");
-		cant_jugadores = this.obtenerEntero(teclado,2,8); //
+		cant_jugadores = Teclado.pedirNumero(2,8); //
 		for (int i=1; i <= cant_jugadores ; i++) {
 			System.out.println("Ingrese el nombre del jugador nro" + i + ": ");
 			String nombre = Teclado.pedirNombre();
@@ -46,25 +45,26 @@ public class Menu {
 	
 	public void gestionarTurnos(Lista<Jugador> jugadores,Tablero tablero,Mazo mazo) {
         jugadores.iniciarCursor(); // Reiniciar el cursor al comienzo de la lista
-        while (this.jugadores.avanzarCursor()) {
-            bool colocarFicha,MoverFicha = false;
+        while (jugadores.avanzarCursor()) {
+            boolean colocarFicha,moverFicha = false;
             Jugador jugadorActual = jugadores.obtenerCursor();
-            jugadorActual.robarCartas(this.tirarDado(),mazo); // tirar dado y robar cartas
+            int numeroAleatorio = this.tirarDado();
+            jugadorActual.robarCartas(numeroAleatorio,mazo); // tirar dado y robar cartas
             System.out.println(jugadorActual.getNombreJugador() + " roba " + numeroAleatorio + " cartas.");
             //Colocar ficha
-            Lista coordAgregar = new Lista();
+            Lista<Integer> coordAgregar = new Lista();
             System.out.println(jugadorActual.getNombreJugador() + " indique en que coordenadas desea colocar su ficha.");
-            this.obtenerCoordenadas(coordenadas,tablero);
+            this.obtenerCoordenadas(coordenadas,tablero); // q haces
             while(!colocarFicha){
                 tablero.colocarFicha(coordenadas.obtenerDato(0),coordenadas.obtenerDato(1),coordenadas.obtenerDato(2), jugadorActual);
             }
             //Mover ficha
-            Lista coordMover1 = new Lista();
+            Lista<Integer> coordMover1 = new Lista();
             System.out.println(jugadorActual.getNombreJugador() + " indique que ficha desea mover de coordenada.");
             obtenerCoordenadas(coordMover1,tablero);
             Casillero coordOrigen = new Casillero(coordMover1.obtenederDato(0),coordMover1.obtenerDato(1),coordMover1.obtenerDato(2));
             System.out.println(jugadorActual.getNombreJugador() + " indique hacia que coordenada desea mover su ficha.");
-            Lista coordMover2 = new Lista();
+            Lista<Integer> coordMover2 = new Lista();
             obtenerCoordenadas(coordMover2,tablero);
             while(!moverFicha){
                 tablero.moverFicha(coordenadas.obtenerDato(0),coordenadas.obtenerDato(1),coordenadas.obtenerDato(2), coordOrigen);
@@ -73,8 +73,8 @@ public class Menu {
             
         }
     }
-            
-    public void generarMazoAleatorio(int cantidadCartas) throws Exception {
+
+    public void generarMazoAleatorio(Lista<Carta> cartas,int cantidadCartas) throws Exception {
         Random random = new Random();
 
         for (int i = 0; i < cantidadCartas; i++) {
@@ -99,14 +99,13 @@ public class Menu {
         }
     }
      
-
     private void jugarCarta(Jugador jugadorActual,Tablero tablero){
-        for (int i=1; i < jugador.cartas.getLongitud();i++){
-            System.out.println("En la posicion " + i + " se tiene la carta " + jugador.cartas.obtenerDato(i));
+        for (int i=1; i < jugadorActual.cartas.getLongitud();i++){ // AAAAAAAAAAA
+            System.out.println("En la posicion " + i + " se tiene la carta " + jugadorActual.cartas.obtenerDato(i));
         }
         System.out.println("Ingrese el numero de la carta que desea utilizar: ");
         int nroCarta = Teclado.obtenerEntero(1,jugador.cartas.getLongitud());
-        Lista coordenadas = new Lista();
+        Lista<Integer> coordenadas = new Lista();
         obtenerCoordenadas(coordenadas,tablero);
         try {
             Carta carta = jugadorActual.getCartas().obtenerDato(nroCarta);
@@ -124,7 +123,7 @@ public class Menu {
     }
 
         
-    private void obtenerCoordenadas(Lista coordenadas,Tablero tablero) {
+    private void obtenerCoordenadas(Lista<Integer> coordenadas,Tablero tablero) {
 	   System.out.println("Ingrese la coordenada X: ");
 	   int x=Teclado.pedirNumero(0,tablero.getTamaño());
 	   coordenadas.agregarElemento(1,x);
@@ -136,16 +135,16 @@ public class Menu {
 	   coordenadas.agregarElemento(3,z);
     }
 
-    private Casillero obtenerCasillero(Lista coordenadas,Tablero tablero){
+    private Casillero obtenerCasillero(Lista<Integer> coordenadas,Tablero tablero){
         this.obtenerCoordenadas(coordenadas,tablero);
         Casillero casillero = new Casillero(coordenadas.obtenerDato(0),coordenadas.obtenerDato(1),coordenadas.obtenerDato(2));
         return casillero;
     }
     
     //limite cartas
-    private int limiteCartas(){
+    public int limiteCartas(){
         System.out.println("Ingrese el limite de cartas con el que desea jugar, no debe superar el tamaño del tablero seleccionado: ");
-        int cantCartas=Teclado.validarEntero(3,99);
+        int cantCartas=Teclado.pedirNumero(3,99);
         return cantCartas; 
     }
 
