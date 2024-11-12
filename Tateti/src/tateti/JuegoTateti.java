4c334f1684411a1c2301ce0b36e1ee851ccbbc6a
jejberowicz <jejberowicz@fi.uba.ar>
2024-11-12 04:06:57 -0300

    Modificaciones en main

--- Tateti/src/tateti/JuegoTateti.java
+++ Tateti/src/tateti/JuegoTateti.java
@@ -1,25 +1,18 @@
 package tateti;
 
 import utilidades.Lista;
-
+import utilidades.Teclado;
 public class JuegoTateti{
     public static void main(String[] args) throws Exception{
     	Teclado teclado =new Teclado(); // scanner
-    	Lista<Jugador> jugadores = new Lista<>(); //iniciamos la lista de jugadores
-    	Menu menu = new Menu(jugadores); // iniciamos al menu con la lista vacia
-    	Mazo mazo = new Mazo(null); //iniciamos mazo !!!!!!!!!! !! ! ! !!!!!!!! !! ! ! 
-    	menu.generarMazoAleatorio(mazo,menu.limiteCartas()); //iniciamos mazo !!!!!!!!!! !! ! ! !!!!!!!! !! ! ! 
-    	
-    	menu.cargarJugadores(); //obtenemos los jugadores
-    	
-        
-        Tablero tablero = new Tablero(menu.obtenerTamonioTablero()); //obtenemos el tamaño del tablero
-		menu.gestionarTurnos(Lista<Jugador> jugadores,Tablero tablero,Mazo mazo)
-        
-
-        //turnos
-        
-        
+    	Lista<Jugador> jugadores = new Lista<>(); //iniciamos jugadores
+    	Menu menu = new Menu(jugadores); // iniciamos menu
+    	Mazo mazo = new Mazo(menu.consultarCantidadCartas()); // generamos el mazo con la cantidad de cartas que los jugadores indiquen   	
+    	menu.cargarJugadores(); //obtenemos los jugadores de 2 a 8 cada uno con un simbolo y color
+        Tablero tablero = new Tablero(menu.obtenerTamonioTablero()); //obtenemos el tamaño del tablero y lo iniciamos
+		menu.gestionarTurnos(jugadores,tablero,mazo); // iniciamos la gestion de turnos
+		// falta modularizar mover fichas y colocar fichas en gestionar turnos, y recorrer los jugadores 
+		// para repartir las cartas antes de cada turno.
         teclado.cerrarScanner();
     }
 }
