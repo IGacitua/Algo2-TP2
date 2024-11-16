706b0f95603c4df775880927d651bad2bb7ee9d5
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-16 00:51:42 -0300

    Mejorados métodos + documentación

--- Tateti/src/utilidades/Teclado.java
+++ Tateti/src/utilidades/Teclado.java
@@ -7,85 +7,104 @@ public class Teclado {
     private static final Scanner teclado = new Scanner(System.in);
     
     /**
-     * pre: -, post: Pide un nombre de usuario por consola.
+     * pre: -, post: Pide un String por consola.
      * Hasta que no se ingrese un nombre valido, no sale del método.
-     * @return Devuelve el nombre ingresado por consola.
+     * @param mensaje Puede especificarse un mensaje al pedir el String, pero
+     * en caso de que no se necesite, puede ser nulo o vacío y se lo reemplaza
+     * por "Ingrese una cadena de texto". 
+     * @return Devuelve la entrada ingresada por consola.
      */
-    public static String pedirNombre() {
+    public static String pedirString(String mensaje) {
+        if ((mensaje.trim().isEmpty()) || (mensaje == null)) {
+            mensaje = "Ingrese una cadena de texto";
+        }
         boolean terminado = false;
-        String nombre = null;
-        System.out.println("Ingrese un nombre: ");
+        String entrada = null;
         while (!terminado) {
-    		nombre = teclado.nextLine();
-    		if (nombre.trim().isEmpty()) {
-    			System.out.println("No ingresó un nombre válido, vuelva a intentarlo.");
-    		} else {
-    			terminado = true;
-    			teclado.next();
-    		}
-    	}
-        return nombre;       
+            System.out.print(mensaje + ": ");
+            entrada = teclado.nextLine();
+            if (entrada.trim().isEmpty()) {
+                System.out.println("No ingresó un valor válido, vuelva a intentarlo.");
+            } else {
+                terminado = true;
+            }
+        }
+        return entrada;
     }
-
+    
     /**
      * pre: -, post: Pide un número por consola.
      * Hasta que no se ingrese un número valido, no sale del método.
+     * @param mensaje Puede especificarse un mensaje al pedir el número, pero
+     * en caso de que no se necesite, puede ser nulo o vacío y se lo reemplaza
+     * por "Ingrese un número".
      * @return Devuelve el número ingresado por consola.
      */
-    public static int pedirNumero() {
-       boolean terminado = false;
-       int numero = 0;
-       System.out.println("Ingrese un número: ");
-       while (!terminado) {
-           if (teclado.hasNextInt()) {
-               numero = teclado.nextInt();
-               terminado = true; 
-           } else {
-               System.out.println("No ingresó un número válido, vuelva a intentarlo.");
-               teclado.next(); 
-           }
-       }
-       return numero;
-   }
+    public static int pedirNumero(String mensaje) {
+    	if ((mensaje.trim().isEmpty()) || (mensaje == null)) {
+    		mensaje = "Ingrese un número";
+    	}
+        boolean terminado = false;
+        int numero = 0;
+        while (!terminado) {
+        	System.out.print(mensaje + ": ");
+            if (teclado.hasNextInt()) {
+                numero = teclado.nextInt();
+                terminado = true; 
+            } else {
+                System.out.println("No ingresó un número válido, vuelva a intentarlo.");
+                teclado.next();  // Limpia el buffer
+            }
+        }
+        teclado.nextLine();
+        return numero;
+    }
+    
     
     /**
      * pre: min y max deben ser válidos,
      * post: Devuelve el numero ingresado por consola.
-     * El número a ingresar debe estar entre min y max.
-     * @param min: Puede ser cualquier número entero. 
-     * @param max: Puede ser cualquier número entero.
+     * El número a ingresar debe estar entre min y max, inclusive para ambos.
+     * @param mensaje Puede especificarse un mensaje al pedir el número, pero
+     * en caso de que no se necesite, puede ser nulo o vacío y se lo reemplaza
+     * por "Ingrese un número".
+     * @param min Puede ser cualquier número entero. 
+     * @param max Puede ser cualquier número entero.
      * @return Devuelve el número ingresado por consola en caso de estar entre min y max.
      * @throws Exception: Si el mínimo y el máximo son iguales.
      */
-    public static int pedirNumero(int min, int max) throws Exception {
-    	if (min == max) {
-    		throw new Exception("El mínimo y el máximo deben ser dos números distintos.");
+    public static int pedirNumeroEntreIntervalo(String mensaje, int min, int max) throws Exception {
+    	if ((mensaje.trim().isEmpty()) || (mensaje == null)) {
+    		mensaje = "Ingrese un número";
     	}
-    	if (min > max) { //TODO: yo acá lanzaría una excepción pero idk
-    		int aux=min;
-    		min=max;
-    		max=aux;
-    	}
+        if (min == max) {
+            throw new Exception("El mínimo y el máximo deben ser dos números distintos.");
+        }
+        if (min > max) { //Si min es mayor que max, intercambia los valores
+            int aux = min;
+            min = max;
+            max = aux;
+        }
         boolean terminado = false;
         int numero = 0;
-        System.out.println("Ingrese un número: ");
         while (!terminado) {
+        	System.out.print(mensaje + " (entre " + min + " y " + max + "): ");
             if (teclado.hasNextInt()) {
                 numero = teclado.nextInt();
-                terminado = true; 
-                if( numero>= min && numero<=max){
-                    terminado=true;
-                    teclado.next();
+                if (numero >= min && numero <= max) {
+                	terminado = true;
+                	//teclado.next();
                 } else {
-                    System.out.println("No ingresaste un numero en el rango permitido, reintente");
+                    System.out.println("El número ingresado no está dentro del rango permitido, vuelva a intentarlo.");
                 }
             } else {
                 System.out.println("No ingresó un número válido, vuelva a intentarlo.");
-                teclado.next(); 
+                teclado.next();  //Limpia el buffer
             }
         }
+        teclado.nextLine();
         return numero;
-     }
+    }
     
     /**
      * pre: -, post: Cierra el teclado.
