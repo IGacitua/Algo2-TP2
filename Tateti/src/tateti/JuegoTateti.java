409185c21a5dc32b8cfc277a424c2d6ee40d2dbd
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-16 11:30:38 -0300

    Actualizado según cambios en Menu

--- Tateti/src/tateti/JuegoTateti.java
+++ Tateti/src/tateti/JuegoTateti.java
@@ -1,20 +1,24 @@
 package tateti;
 
-import utilidades.Lista;
 import utilidades.Teclado;
 
 public class JuegoTateti{
     public static void main(String[] args) throws Exception{
     	Teclado teclado =new Teclado(); // scanner
     	Menu menu = new Menu(); // iniciamos menu
+    	Tablero tablero = Menu.inicializarTablero();
+    	Menu.inicializarJugadores(tablero.getTamaño());
+    	Mazo mazo = Menu.inicializarMazo();
+    	Menu.repartirCartas(mazo);
+    	//Menu.imprimirJugadoresPorPantalla();
     	
-    	Tablero tablero = new Tablero(teclado.pedirNumeroEntreIntervalo("Ingrese el tamaño del tablero con el quiere 				jugar", 3, 99)); 
     	
-    	// generamos el mazo con la cantidad de cartas que los jugadores indiquen
-    	Mazo mazo = new Mazo(menu.consultarCantidadCartas()); 
+    	
+    	//Mazo mazo = new Mazo(menu.consultarCantidadCartas()); 
     	
     	//obtenemos los jugadores de 2 a 8 (como maximo posible) cada uno con un simbolo y color
-    	menu.cargarJugadores(); 
+    	//menu.cargarJugadores(); 
+    	
     	/*   	
     	menu.gestionarTurnos(jugadores,tablero,mazo); // iniciamos la gestion de turnos
        */ 
