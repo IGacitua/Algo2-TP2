8c450b99377c3b0289fc7ee96a7c1770bdee5f8e
Tomas Conti <tconti@fi.uba.ar>
2024-11-18 12:33:33 -0300

    Agregadas validaciones

--- Tateti/src/cartas/CartaBomba.java
+++ Tateti/src/cartas/CartaBomba.java
@@ -17,22 +17,35 @@ public class CartaBomba extends Carta {
 	@Override
 	public void usar(Jugador jugadorActual, Lista<Jugador> listaJugadores, Tablero tablero, Mazo mazo) throws Exception {
 			
-			boolean casilleroValido=false;
-			Casillero casillero=null;
-			System.out.println("Ingrese qué casillero quiere explotar: ");
-			
-			while(!casilleroValido) {	
-				try {
-					 casillero = pedirCasillero(tablero); 
-					casilleroValido=true;
-				}catch(Exception e) {
-					System.out.println(e.getMessage());
-				}
+		if (jugadorActual == null) {
+			throw new Exception("jugadorActual no puede ser null");
+		}
+		if (listaJugadores == null) {
+			throw new Exception("listaJugadores no puede ser null");
+		}
+		if (tablero == null) {
+			throw new Exception("tablero no puede ser null");
+		}
+		if (mazo == null) {
+			throw new Exception("mazo no puede ser null");
+		}
+		
+		boolean casilleroValido=false;
+		Casillero casillero=null;
+		System.out.println("Ingrese qué casillero quiere explotar: ");
+		
+		while(!casilleroValido) {	
+			try {
+				 casillero = pedirCasillero(tablero); 
+				casilleroValido=true;
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
 			}
-			System.out.printf("\n");
-			System.out.println("BOOM!");
-			System.out.printf("\n");
-			this.explotarEntorno(casillero);			
+		}
+		System.out.printf("\n");
+		System.out.println("BOOM!");
+		System.out.printf("\n");
+		this.explotarEntorno(casillero);			
 	}
 
 	/**
