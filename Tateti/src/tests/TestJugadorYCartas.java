31cfc36e0ae5c8271f478d88694cbde09ecc9af5
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-18 16:27:04 -0300

    Update and rename TestJugador.java to TestJugadorYCartas.java

--- Tateti/src/tests/TestJugadorYCartas.java
+++ Tateti/src/tests/TestJugadorYCartas.java
@@ -10,13 +10,13 @@ import tateti.ColoresDisponibles;
 import tateti.Fichas;
 import tateti.Jugador;
 import tateti.Mazo;
+import utilidades.Lista;
 
-//TODO: PROBAR INTERACCIÓN CARTAS-JUGADOR?? no se si algo mas q se me pasa
-public class TestJugador {
+public class TestJugadorYCartas {
 	@Test
 	public void testInicializarNombreInvalido() {
 		Exception exception = assertThrows(Exception.class, () -> {
-	        new Jugador(" ", 4, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO);
+	        new Jugador(" ", 4, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
 		});
 		String mensajeDeErrorEsperado = "El nombre no es válido";
 	    String mensajeDeErrorRecibido = exception.getMessage();
@@ -26,14 +26,14 @@ public class TestJugador {
 	@Test
 	public void testInicializarCantidadesInvalidas() {
 		Exception exception1 = assertThrows(Exception.class, () -> {
-	        new Jugador("Pedro", 0, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO);
+	        new Jugador("Pedro", 0, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
 		});
 		String mensajeDeErrorEsperado1 = "La cantidad de fichas debe ser positiva";
 	    String mensajeDeErrorRecibido1 = exception1.getMessage();
 	    assertTrue(mensajeDeErrorRecibido1.equals(mensajeDeErrorEsperado1));
 	    
 	    Exception exception2 = assertThrows(Exception.class, () -> {
-	    	new Jugador("Pedro", 1, -1, Fichas.CIRCULO, ColoresDisponibles.AMARILLO);
+	    	new Jugador("Pedro", 1, -1, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
 	    });
 	    String mensajeDeErrorEsperado2 = "El valor de cartas máximas debe ser mayor o igual que 0";
 	    String mensajeDeErrorRecibido2 = exception2.getMessage();
@@ -43,9 +43,9 @@ public class TestJugador {
 	@Test
 	public void testEnumFichas() {
 		Exception exception = assertThrows(Exception.class, () -> {
-	        new Jugador("Pedro", 4, 5, null, ColoresDisponibles.AMARILLO);
+	        new Jugador("Pedro", 4, 5, null, ColoresDisponibles.AMARILLO, null);
 		});
-		String mensajeDeErrorEsperado = "La ficha debe existir y estar dentro de las siguientes opciones: CUADRADO, CIRCULO, TRIANGULO, CRUZ, RECTANGULO, ESTRELLA, ROMBO, PAUSA";
+		String mensajeDeErrorEsperado = "La ficha debe existir y estar dentro de las siguientes opciones: CUADRADO, CIRCULO, TRIANGULO, CRUZ, RECTANGULO, ESTRELLA, ROMBO, CORAZON";
 	    String mensajeDeErrorRecibido = exception.getMessage();
 	    assertTrue(mensajeDeErrorRecibido.equals(mensajeDeErrorEsperado));
 	}
@@ -53,24 +53,26 @@ public class TestJugador {
 	@Test
 	public void testEnumColor() {
 		Exception exception = assertThrows(Exception.class, () -> {
-	        new Jugador("Pedro", 4, 5, Fichas.CIRCULO, null);
+	        new Jugador("Pedro", 4, 5, Fichas.CIRCULO, null, null);
 		});
-		String mensajeDeErrorEsperado = "El color debe existir estar dentro de las siguientes opciones: ROJO, VERDE, AZUL, AMARILLO, ROSA, CELESTE, BLANCO, NEGRO";
+		String mensajeDeErrorEsperado = "El color debe existir estar dentro de las siguientes opciones: ROJO, VERDE, AZUL, AMARILLO, ROSA, CELESTE, GRIS, NEGRO";
 	    String mensajeDeErrorRecibido = exception.getMessage();
 	    assertTrue(mensajeDeErrorRecibido.equals(mensajeDeErrorEsperado));
 	}
 	
 	@Test
 	public void testRobarCartas() {
+		Lista<Carta> cartas = new Lista<Carta>();
 		try {
-			Jugador jugador = new Jugador("Juancito", 4, 3, Fichas.CIRCULO, ColoresDisponibles.AMARILLO); //3 es cant cartas MAX
-			Carta[] cartas = new Carta[10];
-			Mazo mazo = new Mazo(cartas);
-			assertTrue(jugador.getCantidadCartas() == 0);
+			Jugador jugador = new Jugador("Juancito", 4, 3, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, cartas); //3: cantCartasMax
+			Mazo mazo = new Mazo(4); //El mazo tendrá 4 cartas de cada tipo.
+			assertTrue(jugador.getCantidadDeCartas() == 0);
 			jugador.robarCartas(2, mazo);
-			assertTrue(jugador.getCantidadCartas() == 2);
+			assertTrue(jugador.getCantidadDeCartas() == 2); //Verifica que tenga dos cartas
 			
-			//intenta sumar dos cartas más cuando ya tiene dos y el máximo es tres
+			System.out.println(jugador.getCartasMaximas());
+			
+			//Intenta sumar dos cartas más cuando ya tiene dos y el máximo es tres
 			Exception exception1 = assertThrows(Exception.class, () -> {
 				jugador.robarCartas(2, mazo);
 			});
@@ -78,7 +80,7 @@ public class TestJugador {
 		    String mensajeDeErrorRecibido1 = exception1.getMessage();
 		    assertTrue(mensajeDeErrorRecibido1.equals(mensajeDeErrorEsperado1));
 			
-			//intenta robar cero cartas
+			//Intenta robar cero cartas
 		    Exception exception2 = assertThrows(Exception.class, () -> {
 		    	jugador.robarCartas(0, mazo);
 			});
@@ -86,7 +88,7 @@ public class TestJugador {
 		    String mensajeDeErrorRecibido2 = exception2.getMessage();
 		    assertTrue(mensajeDeErrorRecibido2.equals(mensajeDeErrorEsperado2));
 		    
-		    //intenta robar sin un mazo del cual hacerlo
+		    //Intenta robar sin un mazo del cual hacerlo
 		    Exception exception3 = assertThrows(Exception.class, () -> {
 		    	jugador.robarCartas(1, null);
 			});
