77808ee0aad446c9fafe8ad15aac04f47e7f74fc
IGacitua <ezmagacitua@gmail.com>
2024-11-18 17:14:43 -0300

    Minima documentación añadida a Casillero

--- Tateti/src/tateti/Casillero.java
+++ Tateti/src/tateti/Casillero.java
@@ -86,6 +86,7 @@ public class Casillero {
         if (casillero == null) {
             throw new Exception("El casillero dado es nulo.");
         }
+        // Busca el casillero en el entorno
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 for (int z = 0; z < 3; z++) {
