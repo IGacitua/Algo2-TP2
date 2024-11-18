7b67cc249b96982e28b3b758805d117705a002aa
Tomas Conti <tconti@fi.uba.ar>
2024-11-18 12:33:46 -0300

    Agregadas validaciones

--- Tateti/src/cartas/CartaVolverTurno.java
+++ Tateti/src/cartas/CartaVolverTurno.java
@@ -12,8 +12,21 @@ public class CartaVolverTurno extends Carta {
      * post: Vuelve hacia atrás un turno completo (una versión anterior del tablero).
      */
 	@Override
-	public void usar(Jugador jugadorActual, Lista<Jugador> listaJugadores, Tablero tablero, Mazo mazo
-			) throws Exception {
+	public void usar(Jugador jugadorActual, Lista<Jugador> listaJugadores, Tablero tablero, Mazo mazo) throws Exception {
+		
+		if (jugadorActual == null) {
+			throw new Exception("jugadorActual no puede ser null");
+		}
+		if (listaJugadores == null) {
+			throw new Exception("listaJugadores no puede ser null");
+		}
+		if (tablero == null) {
+			throw new Exception("tablero no puede ser null");
+		}
+		if (mazo == null) {
+			throw new Exception("mazo no puede ser null");
+		}
+		
 		System.out.printf("\n");
 		System.out.println("Se regresa atras la ronda!");
 		System.out.printf("\n");
