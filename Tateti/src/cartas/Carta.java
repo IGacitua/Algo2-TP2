b6df4813d4ce264fc529e373e2af49b0df273368
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-18 10:58:32 -0300

    Agregada documentación

--- Tateti/src/cartas/Carta.java
+++ Tateti/src/cartas/Carta.java
@@ -10,28 +10,29 @@ import utilidades.Teclado;
 public abstract class Carta {
 	
 	/**
-	 * pre: -
-	 * @param jugadorActual, el jugador que juega la carta
-	 * @param listaJugadores, la lista de jugadores que estan el la partida
-	 * @param tablero, el tablero con el que se desarrolla la partida
-	 * @param mazo, el mazo con el que se juega la partida
-	 * @throws Exception
+	 * pre: jugadorActual, listaJugadores, tablero y mazo deben ser válidos,
+	 * post: Varía según la carta.
+	 * @param jugadorActual El jugador que juega la carta, no debe ser nulo.
+	 * @param listaJugadores La lista de jugadores que están el la partida, no debe ser nula.
+	 * @param tablero El tablero con el que se desarrolla la partida, no debe ser nulo.
+	 * @param mazo El mazo con el que se juega la partida, no debe ser nulo.
+	 * @throws Exception Si alguno de los parámetros es inválido.
 	 */
 	public abstract void usar(Jugador jugadorActual, Lista<Jugador> listaJugadores, Tablero tablero, Mazo mazo) throws Exception;
 	
 	/**
-	 * Pre: - 
-	 * Post: Devuelve el nombre de la carta
+	 * pre: -, post: -
+	 * @return Devuelve el nombre de la carta.
 	 */
 	public abstract String toString();
 	
 	/**
-	 * TODO:
-	 * @param tablero
-	 * @return
+	 * pre: tablero debe ser válido,
+	 * post: Devuelve un Casillero en base a lo ingresado por el usuario.
+	 * @param tablero No debe ser nulo.
+	 * @return Devuelve un Casillero ingresado por el usuario.
 	 */
 	protected Casillero pedirCasillero(Tablero tablero) {
-	
 		boolean valido= false;
 		Casillero casillero=null;
 		
@@ -39,14 +40,11 @@ public abstract class Carta {
 			try {
 				System.out.printf("\n");
 				int x = Teclado.pedirNumeroEntreIntervalo("X", 1, tablero.getTamaño())-1;
-				System.out.printf("\n");
 				int y = Teclado.pedirNumeroEntreIntervalo("Y", 1, tablero.getTamaño())-1;
-				System.out.printf("\n");
 				int z = Teclado.pedirNumeroEntreIntervalo("Z", 1, tablero.getTamaño())-1;
-				System.out.printf("\n");
 				
-				 casillero = tablero.getCasillero(x, y, z);
-				 valido=true;				
+				casillero = tablero.getCasillero(x, y, z);
+				valido=true;				
 				
 			}catch (Exception e) {
 				System.out.println(e.getMessage());
@@ -56,12 +54,12 @@ public abstract class Carta {
 	}
 	
 	/**
-	 * TODO:
-	 * @param listaJugadores
-	 * @return
-	 * @throws Exception
+	 * pre: listaJugadores debe ser válida,
+	 * post: Devuelve el Jugador que fue elegido por consola.
+	 * @param listaJugadores No debe ser nula.
+	 * @return Devuelve un Jugador según la identificación.
 	 */
-	protected Jugador pedirJugadorPorIdentificacion(Lista<Jugador> listaJugadores) throws Exception {
+	protected Jugador pedirJugadorPorIdentificacion(Lista<Jugador> listaJugadores) {
 		Jugador jugadorResultado=null;		
 		int numero=Teclado.pedirNumeroEntreIntervalo(" ", 1, listaJugadores.getLongitud());
 		listaJugadores.iniciarCursor();
@@ -76,11 +74,13 @@ public abstract class Carta {
 	}
 	
 	/**
-	 * TODO:
-	 * @param listaJugadores
+	 * pre: listaJugadores debe ser válida,
+	 * post: Imprime todos los jugadores por pantalla de la forma:
+	 * 1) nombreJugador1, ...
+	 * N) nombreJugadorN
+	 * @param listaJugadores No debe ser nula.
 	 */
 	protected void mostrarJugadores(Lista<Jugador> listaJugadores) {
-		
 		listaJugadores.iniciarCursor();
 		while(listaJugadores.avanzarCursor()) {
 			Jugador jugador=listaJugadores.obtenerCursor();
@@ -89,3 +89,4 @@ public abstract class Carta {
 	}
 	
 }
+
