83bdb04855082d7b6c33448ca3630d045046e2b7
IGacitua <ezmagacitua@gmail.com>
2024-11-18 11:53:16 -0300

    Solucionado crear imagen en blanco?

--- Tateti/src/tateti/Imagen.java
+++ Tateti/src/tateti/Imagen.java
@@ -4,18 +4,18 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import utilidades.Herramientas;
-// TODO ordenar doc
 
 public class Imagen {
 
     //ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
-    private final String RUTA_IMAGENES = "Tp-2/Tateti/src/imagenes/"; // TODO: Eliminar, deberia ser innecesaria
     //ATRIBUTOS -----------------------------------------------------------------------------------------------
     BufferedImage imagen;
 
     //CONSTRUCTORES -------------------------------------------------------------------------------------------
     /**
-     * pre: Recibe una ruta al archivo con la imagen .bmp, post: Crea la Imagen.
+     * pre: Recibe una ruta al archivo con la imagen .bmp
+     *
+     * post: Crea la Imagen.
      *
      * @param archivo: Debe ser una ruta a un archivo .bmp.
      * @throws Exception: Si la ruta dada o el archivo en ésta no son válidos.
@@ -28,12 +28,14 @@ public class Imagen {
     }
 
     /**
-     * pre: Recibe una BufferedImage, post: Crea la Imagen.
+     * pre: Recibe una BufferedImage
+     *
+     * post: Crea la Imagen.
      *
      * @param imagen: No debe ser nulo.
      * @throws Exception: Si la BufferedImage recibida es nula.
      */
-    public Imagen(BufferedImage imagen) throws Exception {
+    private Imagen(BufferedImage imagen) throws Exception {
         if (imagen == null) {
             throw new Exception("La imagen dada es nula.");
         }
@@ -57,6 +59,11 @@ public class Imagen {
             throw new Exception("El alto debe ser mayor a cero. (Es " + alto + ")");
         }
         this.imagen = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < ancho; x++) {
+            for (int y = 0; y < alto; y++) {
+                this.imagen.setRGB(x, y, ((255 << 16) | (255 << 8) | 255));
+            }
+        }
     }
 
     /**
@@ -73,7 +80,7 @@ public class Imagen {
         }
         Jugador jugador = casillero.getJugador();
         if (jugador == null) {
-            throw new Exception("El casillero debe tener un jugador.");
+            throw new Exception("El casillero debe tener una ficha.");
         }
         Fichas fichaJugador = jugador.getFichaImagen();
         this.imagen = ImageIO.read(new File(RUTA_IMAGENES + "shape_" + fichaJugador.ordinal() + ".bmp"));
@@ -82,8 +89,9 @@ public class Imagen {
 
     //METODOS DE CLASE ----------------------------------------------------------------------------------------
     /**
-     * pre: Recibe un nombre de archivo (Sin extensión), post: Convierte la
-     * imagen a un .bmp en destino.
+     * pre: Recibe un nombre de archivo (Sin extensión)
+     *
+     * post: Convierte la imagen a un .bmp en destino.
      *
      * @param destino: No debe ser nulo.
      * @throws Exception: Cuando el nombre de archivo dado no es válido.
@@ -97,8 +105,9 @@ public class Imagen {
     }
 
     /**
-     * pre: Recibe color separado en R G B, post: Convierte todos los pixeles
-     * no-vacios al valor RGB dado.
+     * pre: Recibe color separado en R G B
+     *
+     * post: Convierte todos los pixeles no-vacios al valor RGB dado.
      *
      * @param r: Debe estar entre 0 y 255, inclusive.
      * @param g: Debe estar entre 0 y 255, inclusive.
@@ -124,8 +133,9 @@ public class Imagen {
     }
 
     /**
-     * pre: Recibe un color RGB, post: Convierte todos los pixeles no-vacios al
-     * valor RGB dado.
+     * pre: Recibe un color RGB
+     *
+     * post: Convierte todos los pixeles no-vacios al valor RGB dado.
      *
      * @param RGB: Debe estar entre 0x0 y 0xFFFFFF, inclusive.
      * @throws Exception: Si el valor RGB dado es inválido.
@@ -146,7 +156,7 @@ public class Imagen {
     }
 
     /**
-     * pre: -, post: Convierte la imagen a una matriz RGB.
+     * post: Convierte la imagen a una matriz RGB.
      *
      * @return Devuelve una matriz de pixeles correspondiente a la imagen.
      */
@@ -161,8 +171,9 @@ public class Imagen {
     }
 
     /**
-     * pre: Recibe una matriz de pixeles, post: Devuelve una BufferedImage en
-     * base a la matriz dada.
+     * pre: Recibe una matriz de pixeles
+     *
+     * post: Devuelve una BufferedImage en base a la matriz dada.
      *
      * @param matriz: Debe ser una matriz de pixeles válida.
      * @return Devuelve la imagen del tipo BufferedImage.
@@ -183,9 +194,10 @@ public class Imagen {
     }
 
     /**
-     * pre: Recibe una imagen de la misma altura a esta, post: Añade la imagen
-     * adicional a la derecha de esta imagen. No modifica ninguna de las
-     * imagenes.
+     * pre: Recibe una imagen de la misma altura a esta
+     *
+     * post: Añade la imagen adicional a la derecha de esta imagen. No modifica
+     * ninguna de las imagenes.
      *
      * @param adicional: Debe existir, y ser de la misma altura que esta imagen.
      * @return Devuelve una imagen compuesta. Con la imagen adicional a la
