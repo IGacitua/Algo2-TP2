587407644263f6d03d273c78f876b2507583177a
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-18 19:15:43 -0300

    Cambio Enum a Colores

--- Tateti/src/tests/TestJugadorYCartas.java
+++ Tateti/src/tests/TestJugadorYCartas.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import cartas.Carta;
-import tateti.ColoresDisponibles;
+import tateti.Colores;
 import tateti.Fichas;
 import tateti.Jugador;
 import tateti.Mazo;
@@ -16,7 +16,7 @@ public class TestJugadorYCartas {
 	@Test
 	public void testInicializarNombreInvalido() {
 		Exception exception = assertThrows(Exception.class, () -> {
-	        new Jugador(" ", 4, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
+	        new Jugador(" ", 4, 5, Fichas.CIRCULO, Colores.AMARILLO, null);
 		});
 		String mensajeDeErrorEsperado = "El nombre no es válido";
 	    String mensajeDeErrorRecibido = exception.getMessage();
@@ -26,14 +26,14 @@ public class TestJugadorYCartas {
 	@Test
 	public void testInicializarCantidadesInvalidas() {
 		Exception exception1 = assertThrows(Exception.class, () -> {
-	        new Jugador("Pedro", 0, 5, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
+	        new Jugador("Pedro", 0, 5, Fichas.CIRCULO, Colores.AMARILLO, null);
 		});
 		String mensajeDeErrorEsperado1 = "La cantidad de fichas debe ser positiva";
 	    String mensajeDeErrorRecibido1 = exception1.getMessage();
 	    assertTrue(mensajeDeErrorRecibido1.equals(mensajeDeErrorEsperado1));
 	    
 	    Exception exception2 = assertThrows(Exception.class, () -> {
-	    	new Jugador("Pedro", 1, -1, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, null);
+	    	new Jugador("Pedro", 1, -1, Fichas.CIRCULO, Colores.AMARILLO, null);
 	    });
 	    String mensajeDeErrorEsperado2 = "El valor de cartas máximas debe ser mayor o igual que 0";
 	    String mensajeDeErrorRecibido2 = exception2.getMessage();
@@ -43,7 +43,7 @@ public class TestJugadorYCartas {
 	@Test
 	public void testEnumFichas() {
 		Exception exception = assertThrows(Exception.class, () -> {
-	        new Jugador("Pedro", 4, 5, null, ColoresDisponibles.AMARILLO, null);
+	        new Jugador("Pedro", 4, 5, null, Colores.AMARILLO, null);
 		});
 		String mensajeDeErrorEsperado = "La ficha debe existir y estar dentro de las siguientes opciones: CUADRADO, CIRCULO, TRIANGULO, CRUZ, RECTANGULO, ESTRELLA, ROMBO, CORAZON";
 	    String mensajeDeErrorRecibido = exception.getMessage();
@@ -64,7 +64,7 @@ public class TestJugadorYCartas {
 	public void testRobarCartas() {
 		Lista<Carta> cartas = new Lista<Carta>();
 		try {
-			Jugador jugador = new Jugador("Juancito", 4, 3, Fichas.CIRCULO, ColoresDisponibles.AMARILLO, cartas); //3: cantCartasMax
+			Jugador jugador = new Jugador("Juancito", 4, 3, Fichas.CIRCULO, Colores.AMARILLO, cartas); //3: cantCartasMax
 			Mazo mazo = new Mazo(4); //El mazo tendrá 4 cartas de cada tipo.
 			assertTrue(jugador.getCantidadDeCartas() == 0);
 			jugador.robarCartas(2, mazo);
