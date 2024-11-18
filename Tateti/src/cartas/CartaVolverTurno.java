8df9f68526bf4d301375bc71dc1954680df34da0
Rafaela Arias <131913348+rafaela-arias@users.noreply.github.com>
2024-11-18 10:58:05 -0300

    Agregada documentación

--- Tateti/src/cartas/CartaVolverTurno.java
+++ Tateti/src/cartas/CartaVolverTurno.java
@@ -8,32 +8,30 @@ import utilidades.Lista;
 public class CartaVolverTurno extends Carta {
 	
 	/**
-	 * TODO:
-	 */
+     * Misma documentación de Carta.
+     * post: Vuelve hacia atrás un turno completo (una versión anterior del tablero).
+     */
 	@Override
 	public void usar(Jugador jugadorActual, Lista<Jugador> listaJugadores, Tablero tablero, Mazo mazo
 			) throws Exception {
-		
-		System.out.println("Se regresa atras la ronda");
+		System.out.printf("\n");
+		System.out.println("Se regresa atras la ronda!");
+		System.out.printf("\n");
 		tablero.alternarTablero();
 		listaJugadores.iniciarCursor();
 		while(!listaJugadores.avanzarCursor()) {
 			int fichas=tablero.contarFichas(listaJugadores.obtenerCursor());
 			listaJugadores.obtenerCursor().setCantidadDeFichas(listaJugadores.obtenerCursor().getCantidadDeFichasMaxima()-fichas);
 		}
-		
-		
-		
 	}
 	
 	/**
-	 * TODO:
+	 * pre: -, post: -
+	 * @return Devuelve el nombre de la carta.
 	 */
 	@Override
 	public String toString() {	
 		return "Carta Volver Turno";
 	}
 
-	
-	
 }
